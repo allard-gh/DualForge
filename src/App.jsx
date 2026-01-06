3d0575e6fee97d38e5e0e0cd2f4831360bbc3f67
allard-gh <a.r.snijder@gmail.com>
2026-01-06 10:45:09 +0100

    Basis side bar toegevoegd

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
+import SidebarLayout from './components/layout/SidebarLayout/SidebarLayout.jsx';
 import HomePage from './pages/HomePage/HomePage.jsx';
 import SignInPage from './pages/SignInPage/SignInPage.jsx';
 import SignUpPage from './pages/SignUpPage/SignUpPage.jsx';
@@ -8,14 +9,16 @@ import BuildsPage from './pages/BuildsPage/BuildsPage.jsx';
 
 function App() {
   return (
-    <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/sign-in" element={<SignInPage />} />
-        <Route path="/sign-up" element={<SignUpPage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
-        <Route path="/projects/:projectId" element={<ProjectDetailsPage />} />
-        <Route path="/projects/:projectId/builds" element={<BuildsPage />} />
-    </Routes>
+    <SidebarLayout>
+      <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/sign-in" element={<SignInPage />} />
+          <Route path="/sign-up" element={<SignUpPage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/projects/:projectId" element={<ProjectDetailsPage />} />
+          <Route path="/projects/:projectId/builds" element={<BuildsPage />} />
+      </Routes>
+    </SidebarLayout>
   );
 }
 
---
+++ src/components/layout/SidebarLayout/SidebarLayout.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './SidebarLayout.css';
+
+function SidebarLayout({ children }) {
+  return (
+    <div className="sidebar-layout">
+      <nav className="sidebar">
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/dashboard">Dashboard</NavLink>
+          </li>
+          <li>
+            <NavLink to="/sign-in">Sign In</NavLink>
+          </li>
+          <li>
+            <NavLink to="/sign-up">Sign Up</NavLink>
+          </li>
+        </ul>
+      </nav>
+      <div className="content">
+        {children}
+      </div>
+    </div>
+  );
+}
+
+export default SidebarLayout;
