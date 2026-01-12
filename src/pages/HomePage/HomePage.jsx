d7060c1fdcf13fcd4e608a6377ca181009a6d790
allard-gh <a.r.snijder@gmail.com>
2026-01-12 17:04:03 +0100

    json aangepast en doorverwijzing naar dashboard als nog token aanwezig

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,24 @@
+import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthenticationContext } from "../../context/AuthenticationContext/AuthenticationContext";
 import "./HomePage.css";
 
 function HomePage() {
-    return (
-        <main className="home-page">
-            <h1>Home Page</h1>
-            <p>Welcome to DualForge. Forge your projects with precision.</p>
-        </main>
-    );
+  const { isUserAuthenticated } = useContext(AuthenticationContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isUserAuthenticated) {
+      navigate("/dashboard");
+    }
+  }, [isUserAuthenticated, navigate]);
+
+  return (
+    <main className="home-page">
+      <h1>Home Page</h1>
+      <p>Welcome to DualForge. Forge your projects with precision.</p>
+    </main>
+  );
 }
 
 export default HomePage;
